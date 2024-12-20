126cbaf4c3f8bbd8a1e7feda76f6cc112702eb32
Rishi-42 <np05cp4a190126@iic.edu.np>
2024-12-21 00:37:55 +0545

    Created Custom user model.

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,40 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        email = 'test@example.com'
+        password = 'test@123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password,
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password)) # since the password is hashed we need to use password throuth the hashing sys
+
+    def test_new_user_email_normalized(self):
+        sample_emails = [
+            ['test1@EXAMPLE.COM', 'test1@example.com'],
+            ['Test2@Example.com', 'Test2@example.com'],
+            ['TEST3@EXAMPLE.COM', 'TEST3@example.com'],
+            ['test4@example.COM', 'test4@example.com'],
+        ]
+
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email, 'samplepwd')
+            self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'samplepwd')
+
+    def test_create_superuser(self):
+        user = get_user_model().objects.create_superuser(
+            'test1@example.com',
+            'superpwd',
+        )
+        
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
